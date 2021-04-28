c8bd57a9679282ce6138a78142a20374c656af7b
Raiden <466891291@qq.com>
2021-04-28 16:43:01 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index():
+	return  "<h1>Hello World!!</h1>"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'),500
+
